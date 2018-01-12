69609480f4b25104109c746b433266cf7ecea3c3
Muhammad Hijazi (MBP, Touch) <hijazi@irekasoft.com>
2018-01-12 17:49:23 +0800

    first commit

---
+++ app/App.js
@@ -0,0 +1,81 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS,
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+import SearchPage from './screens/SearchPage'
+import SearchResults from './screens/SearchResults';
+import ItemDetail from './screens/ItemDetail';
+
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+const RootNavigator = StackNavigator({
+  Home: {
+    screen: SearchPage,
+    navigationOptions: {
+      headerTitle: 'Property Finder',
+    },
+  },
+
+  SearchResults: {
+    screen: SearchResults,
+    navigationOptions: {
+      headerTitle: 'Results',
+    },
+  },
+
+  ItemDetail: {
+    screen: ItemDetail,
+  },
+  
+});
+
+
+export default RootNavigator;
+
+
+// export default class App extends Component<{}> {
+
+//   render(){
+//     return (
+//       <NavigatorIOS 
+//       style={styles.container}
+//       initialRoute={{
+//         title: 'Property Finder',
+//         component: SearchPage,
+//       }}/>
+//     );
+//   }
+// }
+
+const styles = StyleSheet.create({
+
+  description: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    justifyContent: 'center',
+    marginTop: 65,
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+
+  container: {
+    flex: 1,
+  },
+  
+});
---
+++ app/config/router.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+
