fb2a3f0b4edd7eb9128249a8a138f38d1b8c8798
Muhammad Hijazi (MBP, Touch) <hijazi@irekasoft.com>
2018-02-19 00:21:18 +0800

    code clean up

--- app/App.js
+++ app/App.js
@@ -44,21 +44,6 @@ const RootNavigator = StackNavigator({
   
 });
 
-
-// export default class App extends Component<{}> {
-
-//   render(){
-//     return (
-//       <NavigatorIOS 
-//       style={styles.container}
-//       initialRoute={{
-//         title: 'Property Finder',
-//         component: SearchPage,
-//       }}/>
-//     );
-//   }
-// }
-
 const styles = StyleSheet.create({
 
   description: {
